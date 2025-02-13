77b3ecbf56c4a4bc21837a3ed8223be36b9a186f
BlairGW <blairgolds@outlook.com>
2025-02-13 11:58:14 +0000

    add delete

--- lab_13/server.js
+++ lab_13/server.js
@@ -60,3 +60,9 @@ app.post('/search', function(req, res) {
     });
 });
 
+app.post('/delete', function(req, res) {
+    db.collection('quotes').deleteOne(req.body, function(err, result) {
+    if (err) throw err;
+    res.redirect('/');
+    });
+});
