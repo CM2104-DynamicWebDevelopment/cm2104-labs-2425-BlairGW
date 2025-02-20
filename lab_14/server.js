386229bc6f25499ec4cb2a1d4686d7144f37408b
BlairGW <blairgolds@outlook.com>
2025-02-20 10:58:45 +0000

    fixed

--- lab_14/server.js
+++ lab_14/server.js
@@ -1,20 +1,20 @@
-// // server.js
-// // load the things we need
-// var express = require('express');
-// var app = express();
-// // set the view engine to ejs
-// app.set('view engine', 'ejs');
-// // use res.render to load up an ejs view file
-// // index page
-// app.get('/', function(req, res) {
-// res.render('pages/index');
-// });
-// // about page
-// app.get('/about', function(req, res) {
-// res.render('pages/about');
-// });
-// app.listen(8080);
-// console.log('8080 is the magic port');
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// use res.render to load up an ejs view file
+// index page
+app.get('/', function(req, res) {
+res.render('pages/index');
+});
+// about page
+app.get('/about', function(req, res) {
+res.render('pages/about');
+});
+app.listen(8080);
+console.log('8080 is the magic port');
 
 // // index page
 // app.get('/', function(req, res) {
