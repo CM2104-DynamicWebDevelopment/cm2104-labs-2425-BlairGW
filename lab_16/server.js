f930f60f9912b8e36021492168e6925770c7fcc7
BlairGW <blairgolds@outlook.com>
2025-03-20 11:01:53 +0000

    small change

--- lab_16/server.js
+++ lab_16/server.js
@@ -16,7 +16,7 @@ io.on('connection', function (socket) {
     console.log('user disconnected');
     });
     socket.on('chat message', function (msg){
-        io.emit('chat message', msg);
+        io.emit('message:', msg);
     });
 });
 
