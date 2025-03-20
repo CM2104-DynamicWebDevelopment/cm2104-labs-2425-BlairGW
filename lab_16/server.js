7d2025c22edc96295c9213d3d5b1e10cb62e5eef
BlairGW <blairgolds@outlook.com>
2025-03-20 10:20:09 +0000

    server

--- lab_16/server.js
+++ lab_16/server.js
@@ -2,26 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
-app.use(express.static('public'));
-
+//code to define the public "static" folder
+app.use(express.static('public'))
+// set the view engine to ejs
 app.set('view engine', 'ejs');
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
 res.render('pages/index');
 });
 
 io.on('connection', function (socket) {
-console.log('A user connected');
-
-socket.on('disconnect', function () {
-console.log('User disconnected');
-});
-
-socket.on('chat message', function (data) {
-io.emit('chat message', data); // Broadcast the username + message
-});
-});
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+    console.log('user disconnected');
+    });
+    });
 
 http.listen(8080, function(){
-console.log('listening on port 8080');
+console.log('listening on *:8080');
 });
